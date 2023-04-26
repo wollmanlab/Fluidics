cfbb9e105a324a9b5ae5d4bafeeaa4d3f87dff82
Wollman Lab code repos <wollmanlab@ucla.edu>
2023-04-26 16:57:01 -0700

    Update DevFluidics.py

--- DevFluidics.py
+++ DevFluidics.py
@@ -23,7 +23,7 @@ class DevFluidics(Fluidics):
         self.Pump.wait_factor = 1/2
         self.Pump.speed_conversion = 1.9
         self.Protocol.speed = 1
-        self.Protocol.closed_speed = 0.25
+        self.Protocol.closed_speed = 0.3
         self.Protocol.wait_factor = self.Pump.wait_factor
         self.Protocol.speed_conversion = self.Pump.speed_conversion
         self.Protocol.rinse_volume = 2.5
