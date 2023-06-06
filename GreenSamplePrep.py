224598d7e56cca69f5ce470e3bca0200935da4d4
Wollman Lab code repos <wollmanlab@ucla.edu>
2023-06-06 08:17:58 -0700

    Update GreenSamplePrep.py

--- GreenSamplePrep.py
+++ GreenSamplePrep.py
@@ -11,7 +11,8 @@ class GreenSamplePrep(Fluidics):
         super().__init__()  # call __init__ method of the super class
         self.verbose = True
         Protocol = getattr(importlib.import_module('SyringeProtocol'), 'SyringeProtocol')
-        Pump = getattr(importlib.import_module('SyringePump'), 'SyringePump')
+        # Pump = getattr(importlib.import_module('SyringePump'), 'SyringePump')
+        Pump = getattr(importlib.import_module('SyringePump_v2'), 'SyringePump_v2')
         Valve = getattr(importlib.import_module('ViciValve'), 'ViciValve')
         self.Protocol = Protocol(gui=gui)
         self.Pump = Pump('/dev/ttyACM0',gui=gui)
@@ -21,13 +22,13 @@ class GreenSamplePrep(Fluidics):
         self.Pump.device = self.device
         self.Valve.device = self.device
         self.Pump.wait_factor = 1/2
-        self.Pump.speed_conversion = 1.9
-        self.Protocol.speed = 1
-        self.Protocol.max_speed = 1
+        self.Pump.speed_conversion = 1.9*(5/4)
+        self.Protocol.speed = 0.5
+        self.Protocol.max_speed = 0.5
         self.Protocol.closed_speed = 0.25
         self.Protocol.wait_factor = self.Pump.wait_factor
         self.Protocol.speed_conversion = self.Pump.speed_conversion
-        self.Protocol.rinse_volume = 2.5
+        self.Protocol.rinse_volume = 2
 
         self.Valve_Commands = {
                                 'A':{'valve':1,'port':1},
@@ -42,8 +43,7 @@ class GreenSamplePrep(Fluidics):
                                 'MOPS':{'valve':1,'port':22},
                                 'TPERM':{'valve':1,'port':21},
                                 'Formamide':{'valve':1,'port':20},
-                                'EthyleneCarbonate':{'valve':1,'port':19},
-                                'ProtKSDS':{'valve':1,'port':18},
+                                'Acrylamide':{'valve':1,'port':19},
                                 'ProtK':{'valve':1,'port':17},
                                 'MelphaX':{'valve':1,'port':16},
                             }
