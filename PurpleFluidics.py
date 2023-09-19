e7c5d1f0b2963fd3dedf261cee5d198918299e06
Wollman Lab code repos <wollmanlab@ucla.edu>
2023-09-19 13:56:14 -0700

    Update PurpleFluidics.py

--- PurpleFluidics.py
+++ PurpleFluidics.py
@@ -1,15 +1,15 @@
 
 from Fluidics import *
 from Fluidics import Fluidics
-from Pumps.SyringePump import SyringePump as Pump
-from Protocols.SyringeProtocol import SyringeProtocol as Protocol
-from Valves.ViciValve import ViciValve as Valve
 
 
 class PurpleFluidics(Fluidics):
     def __init__(self,gui=False):
         super().__init__()  # call __init__ method of the super class
         self.verbose = True
+        Protocol = getattr(importlib.import_module('SyringeProtocol'), 'SyringeProtocol')
+        Pump = getattr(importlib.import_module('SyringePump_v2'), 'SyringePump_v2')
+        Valve = getattr(importlib.import_module('ViciValve'), 'ViciValve')
         self.Protocol = Protocol(gui=gui)
         self.Pump = Pump('COM4',gui=gui)
         self.Valve = Valve('COM3',gui=gui)
@@ -23,43 +23,53 @@ class PurpleFluidics(Fluidics):
         self.Protocol.closed_speed = 0.25
         self.Protocol.wait_factor = self.Pump.wait_factor
         self.Protocol.speed_conversion = self.Pump.speed_conversion
-        self.Valve_Commands = {'TBS':{'valve':2,'port':6},
-                                'IBuffer':{'valve':2,'port':9},
-                                'WBuffer':{'valve':2,'port':7},
-                                'TCEP':{'valve':2,'port':8},
-                                'Waste':{'valve':3,'port':10},
-                                'A':{'valve':3,'port':2},
-                                'B':{'valve':3,'port':3},
-                                'C':{'valve':3,'port':4},
-                                'D':{'valve':3,'port':5},
-                                'E':{'valve':3,'port':6},
-                                'F':{'valve':3,'port':7},
-                                'M':{'valve':3,'port':8},
-                                'Hybe1':{'valve':1,'port':1},
-                                'Hybe2':{'valve':1,'port':2},
-                                'Hybe3':{'valve':1,'port':3},
-                                'Hybe4':{'valve':1,'port':4},
-                                'Hybe5':{'valve':1,'port':5},
-                                'Hybe6':{'valve':1,'port':6},
-                                'Hybe7':{'valve':1,'port':7},
-                                'Hybe8':{'valve':1,'port':8},
-                                'Hybe9':{'valve':1,'port':9},
-                                'Hybe10':{'valve':1,'port':10},
-                                'Hybe11':{'valve':1,'port':11},
-                                'Hybe12':{'valve':1,'port':12},
-                                'Hybe13':{'valve':1,'port':13},
-                                'Hybe14':{'valve':1,'port':14},
-                                'Hybe15':{'valve':1,'port':15},
-                                'Hybe16':{'valve':1,'port':16},
-                                'Hybe17':{'valve':1,'port':17},
-                                'Hybe18':{'valve':1,'port':18},
-                                'Hybe19':{'valve':1,'port':19},
-                                'Hybe20':{'valve':1,'port':20},
-                                'Hybe21':{'valve':1,'port':21},
-                                'Hybe22':{'valve':1,'port':22},
-                                'Hybe23':{'valve':1,'port':23},
-                                'Hybe24':{'valve':1,'port':24},
-                                'Hybe25':{'valve':2,'port':5},
-                                'Valve1':{'valve':2,'port':10},
-                                'Valve2':{'valve':3,'port':1}
+        self.Protocol.vacume = True
+        self.Protocol.hybe_time = 10*60
+        self.Valve_Commands = {'TBS':{'valve':3,'port':6},
+                                'IBuffer':{'valve':3,'port':9},
+                                'WBuffer':{'valve':3,'port':7},
+                                'TCEP':{'valve':3,'port':8},
+                                'Waste':{'valve':4,'port':10},
+                                'A':{'valve':4,'port':2},
+                                'B':{'valve':4,'port':3},
+                                'C':{'valve':4,'port':4},
+                                'D':{'valve':4,'port':5},
+                                'E':{'valve':4,'port':6},
+                                'F':{'valve':4,'port':7},
+                                'M':{'valve':4,'port':8},
+                                'Hybe1':{'valve':2,'port':1},
+                                'Hybe2':{'valve':2,'port':2},
+                                'Hybe3':{'valve':2,'port':3},
+                                'Hybe4':{'valve':2,'port':4},
+                                'Hybe5':{'valve':2,'port':5},
+                                'Hybe6':{'valve':2,'port':6},
+                                'Hybe7':{'valve':2,'port':7},
+                                'Hybe8':{'valve':2,'port':8},
+                                'Hybe9':{'valve':2,'port':9},
+                                'Hybe10':{'valve':2,'port':10},
+                                'Hybe11':{'valve':2,'port':11},
+                                'Hybe12':{'valve':2,'port':12},
+                                'Hybe13':{'valve':2,'port':13},
+                                'Hybe14':{'valve':2,'port':14},
+                                'Hybe15':{'valve':2,'port':15},
+                                'Hybe16':{'valve':2,'port':16},
+                                'Hybe17':{'valve':2,'port':17},
+                                'Hybe18':{'valve':2,'port':18},
+                                'Hybe19':{'valve':2,'port':19},
+                                'Hybe20':{'valve':2,'port':20},
+                                'Hybe21':{'valve':2,'port':21},
+                                'Hybe22':{'valve':2,'port':22},
+                                'Hybe23':{'valve':2,'port':23},
+                                'Hybe24':{'valve':2,'port':24},
+                                'Hybe25':{'valve':3,'port':5},
+                                'Valve2':{'valve':3,'port':1},
+                                'Valve3':{'valve':4,'port':1},
+                                'Vacume_Waste':{'valve':1,'port':10},
+                                'Vacume_A':{'valve':1,'port':2},
+                                'Vacume_B':{'valve':1,'port':3},
+                                'Vacume_C':{'valve':1,'port':4},
+                                'Vacume_D':{'valve':1,'port':5},
+                                'Vacume_E':{'valve':1,'port':6},
+                                'Vacume_F':{'valve':1,'port':7},
+                                'Vacume_M':{'valve':1,'port':8},
                             }
